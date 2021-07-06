10b5d41ac4ffdca1a67d5b5e21491117983c18ef
nulysses <adr.zivil@gmail.com>
2021-07-06 08:50:21 +0200

    1

--- install.sh
+++ install.sh
@@ -32,13 +32,11 @@ set -o pipefail
 set -eu
 #################################SCRIPT_START##################################
 
-set -x
+ar18.script.import ar18.script.install
 
 . "${script_dir}/vars"
-if [ ! -v ar18_helper_functions ]; then rm -rf "/tmp/helper_functions_$(whoami)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(whoami)"; . "/tmp/helper_functions_$(whoami)/helper_functions/helper_functions.sh"; cd "${script_dir}"; export ar18_helper_functions=1; fi
-obtain_sudo_password
 
-ar18_install "${install_dir}" "${module_name}" "${script_dir}"
+ar18.script.install "${install_dir}" "${module_name}" "${script_dir}"
 
 ##################################SCRIPT_END###################################
 # Restore old shell values
